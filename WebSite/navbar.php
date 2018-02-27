f848a1b6ee802fdd19b03fb7ed34d0bee89a563a
Mile <Mile@DESKTOP-K72BU75>
2018-02-27 20:18:47 +0000

    google anal

--- WebSite/navbar.php
+++ WebSite/navbar.php
@@ -73,3 +73,12 @@
 <script>
     <?php include 'scripts/navscript.js'; ?>
 </script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-114898136-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-114898136-1');
+</script>
