2b61f33c4c549260ac88b5441c1d41e94be224ca
stojancelevski <stojancelevski@users.noreply.github.com>
2018-03-05 12:09:16 +0100

    safe

--- WebSite/safe and regular.php
+++ WebSite/safe and regular.php
@@ -4,14 +4,12 @@
                 boost
             </h1>
             <li class="list-group-item sina">Safe boost</li>
-            <li class="list-group-item">We wanted to give you the safest experience so that's why we have created a
-                specific
-                way of boosting so that you can feel safer and at ease.
+            <li class="list-group-item">We wanted to give you the safest league of legends elo boost experience so
+                that's why we have created a specific way of elo boosting so that you can feel safer and at ease.
             </li>
             <li class="list-group-item sina">Regular boost</li>
-            <li class="list-group-item">This boost only uses a VPN from the boosters side, and they provide regular
-                boosting
-                process.
+            <li class="list-group-item">This league of legends elo boost only uses a VPN from the boosters side, and
+                they follow regular elo boosting process.
             </li>
         </ul>
     </div>
@@ -22,13 +20,13 @@
     <div class="col-md-8">
         <ul class="list-group"><h1 style="text-align: center;color: white;">Full 100% Boost Control and Customization
             </h1>
-            <li class="list-group-item sina">Top Boost gives you 100% control over your boost so you be in charge of
-                your boost by being able to pause, change your booster, spectate and fully customize the heroes that
-                will be played (min 3 max 10).
+            <li class="list-group-item sina">Top Boost gives you 100% control over your boost (eke. every elo boosted
+                players) so you will be in charge of your elo boost by being able to pause, change your elo booster,
+                spectate and fully customize the heroes that will be played (min 3 max 10), no additional charges.
             </li>
             <div class="list-group-item">
-                <li>Pausing is done by contacting your booster or our team through the contact form.</li>
-                <li>You can change or pick the booster by telling your boster or contacting our team by using the
+                <li>Pausing is done by contacting your friendly booster or our team through the contact form.</li>
+                <li>You can change or pick the booster by telling your booster or contacting our team by using the
                     contact
                     form after your purchase.
                 </li>
@@ -36,9 +34,9 @@
                     will
                     guide you through it.
                 </li>
-                <li>Full hero customization is done through the input form or after the purchase through the contact
+                <li>Full free hero selection is done through the input form or after the purchase through the contact
                     form,
-                    and you can also tell your boster about it.
+                    and you can also tell your booster about it.
                 </li>
             </div>
         </ul>
@@ -47,11 +45,16 @@
 <br>
 <div class="row">
     <div class="col-md-8">
-        <ul class="list-group"><h1 style="text-align: center;color: white;">Reliable and Secure
+        <ul class="list-group"><h1 style="text-align: center;color: white;">Reliable, Secure and fast lol elo boost
+                service
             </h1>
-            <li class="list-group-item sina">Top boost provides you with a safe and secure experience with every boost
-                you purchase. Our professional players are highly adaptable, they are flexible and most importantly they
-                are experienced in climbing ranks efficiently.
+            <li class="list-group-item sina">Top Boost provides you with a safe and secure experience with every elo
+                boost you purchase. Our professional players are highly adaptable, they are flexible and most
+                importantly they are experienced in climbing ranks efficiently.
+            </li>
+            <li class="list-group-item ">100% Money Back Guarantee</li>
+            <li class="list-group-item sina">When you buy elo boost on our site we guarantee that your cheap elo boost no
+                matter of your region turkey europe russia.... will be refunded.
             </li>
 
         </ul>
